fe23f4d899dd0a29a413a29b1d58c405782ca34a
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-03-29 00:58:52 +0300

    Added header of texture for futher development

---
+++ impl/include/Texture.hpp
@@ -0,0 +1,29 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include <SDL2/SDL.h>
+
+#include "CustomException.hpp"
+
+namespace Backgammon
+{
+
+class Texture
+{
+
+using texture_ptr = SDL_Texture*;
+using texture_t   = SDL_Texture;
+using size_t      = std::size_t;
+
+    SDL_Texture* texture_ = nullptr;
+    size_t width_ = 0, height_ = 0;
+
+public:
+
+    Texture (){}
+    ~Texture(){}
+
+};
+}
+
+#endif
